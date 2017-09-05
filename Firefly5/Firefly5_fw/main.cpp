124b8824cdd874282f42082fee12b5b4bc395f79
Kreyl <laystoll@yandex.ru>
2017-09-05 20:20:39 +0300

    + Firefly5 OS works

---
+++ Firefly5/Firefly5_fw/main.cpp
@@ -0,0 +1,69 @@
+#include "hal.h"
+#include "MsgQ.h"
+#include "shell.h"
+#include "uart.h"
+#include "SimpleSensors.h"
+#include "buttons.h"
+#include "board.h"
+
+#define FLASH_DURATION  45
+#define LED_DAC_VALUE   1600    // 1A
+
+// Forever
+EvtMsgQ_t<EvtMsg_t, MAIN_EVT_Q_LEN> EvtQMain;
+extern CmdUart_t Uart;
+void OnCmd(Shell_t *PShell);
+void ITask();
+
+int main(void) {
+    // ==== Init Clock system ====
+    Clk.UpdateFreqValues();
+
+    // === Init OS ===
+    halInit();
+    chSysInit();
+
+    // ==== Init hardware ====
+    EvtQMain.Init();
+    Uart.Init(115200);
+    Printf("\r%S %S\r", APP_NAME, BUILD_TIME);
+    Clk.PrintFreqs();
+
+//    SimpleSensors::Init();
+
+    // Main cycle
+    ITask();
+}
+
+__noreturn
+void ITask() {
+    while(true) {
+        EvtMsg_t Msg = EvtQMain.Fetch(TIME_INFINITE);
+        switch(Msg.ID) {
+            case evtIdShellCmd:
+                OnCmd((Shell_t*)Msg.Ptr);
+                ((Shell_t*)Msg.Ptr)->SignalCmdProcessed();
+                break;
+
+            case evtIdButtons:
+                Printf("Btn\r");
+
+                break;
+
+            default: break;
+        } // switch
+    } // while true
+}
+
+#if 1 // ======================= Command processing ============================
+void OnCmd(Shell_t *PShell) {
+    Cmd_t *PCmd = &PShell->Cmd;
+//    __unused int32_t dw32 = 0;  // May be unused in some configurations
+//    Uart.Printf("\r%S\r", PCmd->Name);
+    // Handle command
+    if(PCmd->NameIs("Ping")) PShell->Ack(retvOk);
+    else if(PCmd->NameIs("Version")) PShell->Printf("%S %S\r", APP_NAME, BUILD_TIME);
+
+    else PShell->Ack(retvCmdUnknown);
+}
+#endif
